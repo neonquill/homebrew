b62fa675c16b8c2da1901aa489d1652907db7d5b
David Watson <david@neonquill.com>
2011-11-10 18:33:46 -0500

    Adding a patch to deal with usb timeouts.
    Fixes a problem with an AVRISP mkII and OSX.  Not sure why the underlying
    usb read fails, but this timeout significantly reduces the time to program.

--- Library/Formula/avrdude.rb
+++ Library/Formula/avrdude.rb
@@ -11,6 +11,11 @@ class Avrdude < Formula
     [['--with-usb', 'Compile AVRDUDE with USB support.']]
   end
 
+  def patches
+    # Reduce usb_bulk_read timeout.
+    { :p2 => "https://github.com/neonquill/avrdude/commit/b0d39fea608464a599cc2087ed18f0b5159ad8c5.diff"}
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
